72ed1bb1826c985d818e9fdf15509c375819d409
Peter Alexander <makeroftools@creativedevelopment.systems>
2022-01-17 16:53:20 -0500

    fixed naming

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'cdt-stencil-apollo',
+  namespace: 'stencil-apollo',
   outputTargets: [
     {
       type: 'dist',
